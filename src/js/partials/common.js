57a140170b87b35c0a3654d3f4318d2361be48b4
Daniil Postnov <dpostnov@ronasit.com>
2019-07-25 17:53:24 +0300

    finish styles

--- src/js/partials/common.js
+++ src/js/partials/common.js
@@ -1,16 +1,4 @@
-console.log('i am work!', 'Change this message in "src/js/partails/common.js"');
-
-// This template support es6 syntax
-
-// let, destructurization
-let arr = ['Postnov', 'Daniil'],
-  [firstName, lastName] = arr,
-  func = (message) => console.log(message);
-
-
-console.log('Author this template: ', firstName, lastName);
-
-// const, template string
-const str = `Hello, i am ${firstName} ${lastName} from console and arrow function`;
-
-func(str);
+var faqList = document.querySelector('.faq-list');
+var msnry = new Masonry( faqList, {
+  itemSelector: '.faq-masonry',
+});
